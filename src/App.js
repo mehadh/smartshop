cd046fbbbc621719be752f4c16b6f29227ccaa3c
Mussie <mussieabraham03@gmail.com>
2023-06-26 16:09:53 -0400

    switching to react js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+
+const BackgroundImagePage = () => {
+  return (
+    <div className="SmartShop"></div>
+  );
+}
+
+
+export default BackgroundImagePage;
