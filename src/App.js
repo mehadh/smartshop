783075c1262c032fe88f9c8b203ef5e85f70ca38
Mussie <mussieabraham03@gmail.com>
2023-06-27 15:24:01 -0400

    search bar added and framework for api use for search bar later

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { useState } from "react";
 import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App(){
+
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  const onSearch= (searchTerm) => {
+    // our api to fetch search result
+    console.log('search ', searchTerm);
+  }
+
   return (
     <div className ="App">
       <Router>
@@ -13,7 +26,12 @@ function App(){
         </Routes>
         
       </Router>
+      <div className= "search">
+        <input type="text" value={value} onChange={onChange}/>
+        <button onClick={() => onSearch(value)}>Search</button>
+      </div>
     </div>
+      
 
   );
 }
