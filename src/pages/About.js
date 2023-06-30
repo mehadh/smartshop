ccfe477f492787bd1edba3797bb29d65e990701e
rashrahman <90875950+rashrahman@users.noreply.github.com>
2023-06-30 12:38:56 -0400

    made the about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import Logo from "../images/logo.png";
+import grocery from "../images/grocery.webp";
 import "../styles/About.css";
+
 
 function About() {
   return (
     <div className="about">
         <div className = "aboutTop" 
-       
-      > </div>
+       style ={{ backgroundImage: `url(${grocery})`}}>
+       </div>
         <div className = "aboutBottom">
             <h1> ABOUT US</h1>
             <p>
-            SmartShop is a powerful online platform designed to revolutionize your 
-            grocery shopping experience. Our website provides an intuitive and convenient 
-            way to compare prices of grocery items within your specified radius. Say goodbye 
-            to endlessly browsing through different store flyers or wasting time visiting 
+            At SmartShop, we believe in making the most out of your grocery shopping experience. 
+            SmartShop is an online platform designed to revolutionize your grocery trip. Our website 
+            provides an intuitive and convenient way to compare prices of grocery items in the Greater Boston area. 
+            Say goodbye to endlessly browsing through different store flyers or wasting time visiting 
             multiple stores to find the best deals. With SmartShop, you can effortlessly find 
             the best prices on your favorite grocery items, all in one place. Using our user-friendly
-             interface, simply input the items you need and select your desired radius. SmartShop will 
+             interface, simply input the items you need one at a time. SmartShop will 
              then scan the databases of various local grocery stores, allowing you to quickly view and 
              compare prices. Whether you're looking for fresh produce, pantry staples, or household 
              essentials, SmartShop ensures that you always get the best value for your money. 
@@ -26,6 +27,7 @@ function About() {
              With just a few clicks, you can make informed decisions and plan your shopping trips 
              efficiently. We believe that grocery shopping should be hassle-free, cost-effective, 
              and enjoyable, which is why we created SmartShop.
+
             </p>
 
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,11 @@ import "../styles/Home.css";
 import bg from '../images/background.png';
 function Home() {
   return (
-    <div className="home"
-    style = {{ backgroundImage: `url(${bg})`}}>
+    <div className="home"  style ={{ backgroundImage: `url(${bg})`}}>
+      <div className="top"> 
+        <h1> SmartShop</h1>
+        <p> Shopping made easier and cheaper </p>
+      </div>
     </div>
   ); 
 }
