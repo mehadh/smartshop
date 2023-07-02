0bf3d1d68c6f42dc9ba27665f79290775d67807e
Mussie <mussieabraham03@gmail.com>
2023-07-01 21:57:15 -0400

    search bar only on homepage now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/Home.css";
 import bg from '../images/background.png';
 function Home() {
+
+  const [value, setValue] = useState('');
+
+    const onChange = (event) => {
+    setValue(event.target.value);
+    }
+
+    const onSearch= (searchTerm) => {
+    // our api to fetch search result
+    console.log('search ', searchTerm);
+    }
+
   return (
     <div className="home"  style ={{ backgroundImage: `url(${bg})`}}>
       <div className="top"> 
         <h1> SmartShop</h1>
         <p> Shopping made easier and cheaper </p>
+      </div>
+      <div className= "search">
+        <input type="text" value={value} onChange={onChange}/>
+        <button onClick={() => onSearch(value)}>Search</button>
       </div>
     </div>
   ); 
