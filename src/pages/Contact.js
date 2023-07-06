86520f718b49b865eabfd031f3c89a34e9f93920
rashrahman <90875950+rashrahman@users.noreply.github.com>
2023-07-06 17:58:59 -0400

    made the contact page frontend

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import "../styles/Contact.css";
+import grocery from "../images/grocery2.webp";
 function Contact() {
   return (
-    <div>
-    
+    <div className='contact'>
+      <div className='left'
+        style={{backgroundImage: `url(${grocery})`}}
+      >
+
+      </div>
+
+      <div className='right'>
+        <h1> Contact Us </h1>
+        <form id="contact-form" method="POST">
+          <label htmlFor = "name">Your Name</label>
+          <input name="name" placeholder="Enter full name" type ="text"/>
+          <label htmlFor = "email">Email </label>
+          <input name="email" placeholder="Enter email" type ="email"/>
+          <label htmlFor='message'>Message</label>
+          <textarea
+            rows="6"
+            placeholder='Enter message here...'
+            name='message'
+            required></textarea>
+          <button type='submit'>Send Message </button>
+        </form>
+      </div>
+      
     </div>
   )
 }
