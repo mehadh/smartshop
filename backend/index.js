aec286a619798b4bf5e825e1c7929e534f0d4587
Mussie <mussieabraham03@gmail.com>
2023-07-09 15:35:31 -0400

    new stuff

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("Hello!")
+console.log("Backend Started!")
+
+const express = require("express")
+const app = express()
+
+
+app.get('/test', (req, res) => {
+    res.send('Hello!')
+} )
+
+
+
+
+app.listen(3001, () => {
+    console.log("express is listening")
+})
+
--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import axios from "axios";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App(){
+  const data = async() => {
+    try {
+      const response = await axios.get("localhost:3001/test")
+      return response.data
+    }
+    catch(error){
+      console.log(error)
+      return ":("
+    }
+  }
   return (
     
     <div className ="App"> 
@@ -18,6 +29,9 @@ function App(){
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
+      <p>
+        {await data()}
+      </p>
     </div>
 
     
