7ce66d9739db2ddaf2a003f4dbe9ce79fd9c9d5b
rashrahman <90875950+rashrahman@users.noreply.github.com>
2023-07-17 14:33:14 -0400

    added the searchQuery function

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,27 @@ app.listen(3001, () => {
     console.log("express is listening")
 })
 
+//dummy data of products that the search function will use to populate search results
+const products = [
+    {  name: 'Apple', category: 'Fruits' },
+    {  name: 'Banana', category: 'Fruits' },
+    {  name: 'Carrot', category: 'Vegetables' },
+    {  name: 'Tomato', category: 'Vegetables' },
+    {  name: 'Orange', category: 'Fruits' },
+    {  name: 'Potato', category: 'Vegetables' }
+]
+//backend search function
+function searchProducts (query){
+    const lowerCaseQuery = query.toLowerCase(); //making the query case insensitive
+
+    //filtering the products
+    const searchResults = products.filter((product) => {
+        const lowercaseName = product.name.toLowerCase();
+
+        return (lowercaseName.includes(lowerCaseQuery)) 
+    })
+
+    return searchResults
+
+}
+
