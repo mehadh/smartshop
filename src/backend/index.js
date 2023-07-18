4f698e860f891298734d0a8268c48fd15350e5eb
Mussie <mussieabraham03@gmail.com>
2023-07-18 13:29:07 -0400

    working on task 6

---
+++ src/backend/index.js
@@ -0,0 +1,49 @@
+console.log("Backend Started!")
+
+const express = require("express")
+const app = express()
+const cors = require('cors')
+app.use(cors());
+
+
+
+app.get('/test', (req, res) => {
+    res.send('Hello!')
+} )
+
+app.get('/search', (req, res) => {
+    const { query } = req.query;
+    const results = searchProducts(query);
+    res.json(results);
+  });
+
+
+app.listen(3001, () => {
+    console.log("express is listening")
+})
+
+
+//dummy data of products that the search function will use to populate search results
+const products = [
+    {  name: 'Apple', category: 'Fruits' },
+    {  name: 'Banana', category: 'Fruits' },
+    {  name: 'Carrot', category: 'Vegetables' },
+    {  name: 'Tomato', category: 'Vegetables' },
+    {  name: 'Orange', category: 'Fruits' },
+    {  name: 'Potato', category: 'Vegetables' }
+]
+//backend search function
+function searchProducts (query){
+    const lowerCaseQuery = query.toLowerCase(); //making the query case insensitive
+
+    //filtering the products
+    const searchResults = products.filter((product) => {
+        const lowercaseName = product.name.toLowerCase();
+
+        return (lowercaseName.includes(lowerCaseQuery)) 
+    })
+
+    return searchResults
+
+}
+
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,62 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 import "../styles/Home.css";
 import bg from '../images/background.png';
 import logo from '../images/logo.png';
-import { SearchBar } from "../components/SearchBar";
-import { SearchResultsList } from "../components/SearchResultsList";
 
 
 function Home() {
-  const [results, setResults] = useState([]);
+
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  // Function to call the backend search function and update searchResults
+  const handleSearch = async (searchQuery) => {
+    try {
+      const response = await axios.get(`http://localhost:3001/search`, {
+        params: {
+          query: searchQuery
+        }
+      });
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  };
+
+  const handleChange = (event) => {
+    const searchQuery = event.target.value;
+    setQuery(searchQuery);
+    handleSearch(searchQuery);
+  };
+
   return (
-    <div className="home"  style ={{ backgroundImage: `url(${bg})`}}>
-      <div className = "centerLogo"><img src = {logo}/></div>
+    <div className="home" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="centerLogo"><img src={logo} /></div>
       <div className="top">
-        <h1> SmartShop</h1>
-        <p> Shopping made easier and cheaper </p>
+        <h1>SmartShop</h1>
+        <p>Shopping made easier and cheaper</p>
       </div>
-      <SearchBar setResults={setResults} />
-        {results && results.length > 0 && <SearchResultsList results={results} />}
+      <div>
+        <input
+          type="text"
+          value={query}
+          onChange={handleChange}
+          placeholder="Search for products"
+        />
       </div>
+      <div>
+        {/* Display search results here */}
+        {searchResults.map((product, index) => (
+          <div key={index}>
+            <p>{product.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+
   ); 
 }
-export default Home;
+
+export default Home
